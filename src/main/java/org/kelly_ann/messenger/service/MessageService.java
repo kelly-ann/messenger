f819683054b92a33beea802256146f8852860e62
specialk1st <kapeart@gmail.com>
2015-04-17 00:52:53 -0400

    Added service stubs

--- src/main/java/org/kelly_ann/messenger/service/MessageService.java
+++ src/main/java/org/kelly_ann/messenger/service/MessageService.java
@@ -2,7 +2,9 @@ package org.kelly_ann.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.kelly_ann.messenger.database.DatabaseClass;
 import org.kelly_ann.messenger.model.Message;
 
 /*
@@ -13,13 +15,37 @@ import org.kelly_ann.messenger.model.Message;
 // Note: if this were an actual service it would make a call to the db and get the list of Messages.
 public class MessageService {
 	
-	public List<Message> getAllMessages() {
-		Message m1 = new Message(1L, "Hello World!", "Kelly-Ann");
-		Message m2 = new Message(2L, "Hello Jersey!", "Kelly-Ann");
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "Kelly-Ann"));
+		messages.put(2L, new Message(2, "Hello Jersey", "Kelly-Ann"));
 	}
 	
-}
+	public List<Message> getAllMessages() {
+		// pass the Map collection to the ArrayList collection to initialize the ArrayList with those elements.
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+} 
